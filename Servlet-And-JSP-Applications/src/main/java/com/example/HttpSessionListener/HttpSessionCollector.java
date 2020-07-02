15131ba1faf70a3b1debe4ded7976835d808284f
prakash-gangadharan <prakash08.g@gmail.com>
2020-07-02 15:38:28 +0530

    maven changes

--- Servlet-And-JSP-Applications/src/main/java/com/example/HttpSessionListener/HttpSessionCollector.java
+++ Servlet-And-JSP-Applications/src/main/java/com/example/HttpSessionListener/HttpSessionCollector.java
@@ -5,13 +5,11 @@ import javax.servlet.http.HttpSessionListener;
 
 public class HttpSessionCollector implements HttpSessionListener{
 
-	@Override
 	public void sessionCreated(HttpSessionEvent event) {
 		System.out.println("session created");
 		
 	}
 
-	@Override
 	public void sessionDestroyed(HttpSessionEvent event) {
 		
 		System.out.println("session Destroyed");
